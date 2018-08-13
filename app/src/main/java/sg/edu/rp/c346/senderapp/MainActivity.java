c15c32c5c5ecfda4d638333f2e36165edda63c15
simply-hh <16039009@myrp.edu.sg>
2018-08-13 11:46:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/senderapp/MainActivity.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.senderapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcast.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
